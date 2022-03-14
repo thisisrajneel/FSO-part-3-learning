9e84155cb3fc8bef3c89a92288e60dfbb991489a
Rajneel Anuraj <rajneelanuraj2001@gmail.com>
2022-03-14 17:16:55 +0530

    Heroku

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ app.post('/api/notes', (req,res) => {
     res.json(note)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`);
 })
