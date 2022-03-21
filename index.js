0d56840452e80de433a114f0103749688205340d
Rajneel Anuraj <rajneelanuraj2001@gmail.com>
2022-03-21 10:23:24 +0530

    using db in route handlers

--- index.js
+++ index.js
@@ -51,14 +51,9 @@ app.get('/api/notes', (req,res) => {
 })
 
 app.get('/api/notes/:id', (req,res) => {
-    const id = Number(req.params.id)
-    const note = notes.find(n => n.id === id)
-    if(note) {
+    Note.findById(req.params.id).then(note => {
         res.json(note)
-    }
-    else {
-        res.status(404).end()
-    }
+    })
 })
 
 app.delete('/api/notes/:id', (req,res) => {
@@ -67,11 +62,6 @@ app.delete('/api/notes/:id', (req,res) => {
 
     res.status(204).end()
 })
-
-const generateId = () => {
-    const maxId = notes.length > 0 ? Math.max(...notes.map(note => note.id)) : 0
-    return maxId+1
-}
 
 app.post('/api/notes', (req,res) => {
     const body = req.body
@@ -82,15 +72,15 @@ app.post('/api/notes', (req,res) => {
         })
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
         important: body.important || false,
         date: new Date(),
-        id: generateId()
-    }
+    })
     
-    notes = notes.concat(note)
-    res.json(note)
+    note.save().then(savedNote=> {
+        res.json(savedNote)
+    })
 })
 
 const PORT = process.env.PORT
