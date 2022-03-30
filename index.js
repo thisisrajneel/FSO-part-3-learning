fd08f070576aaaea73b0d713aef5efc666eae01d
Rajneel Anuraj <rajneelanuraj2001@gmail.com>
2022-03-30 15:17:43 +0530

    added linting

--- index.js
+++ index.js
@@ -10,113 +10,113 @@ app.use(express.static('build'))
 app.use(cors())
 
 const requestLogger = (req,res,next) => {
-    console.log('Method: ',req.method);
-    console.log('Path: ', req.path);
-    console.log('Body: ', req.body);
-    console.log('---');
-    next()
+  console.log('Method: ',req.method)
+  console.log('Path: ', req.path)
+  console.log('Body: ', req.body)
+  console.log('---')
+  next()
 }
 
 app.use(requestLogger)
 
 const errorHandler = (error, req, res, next) => {
-    console.error(error.message);
+  console.error(error.message)
 
-    if(error.name === 'CastError') {
-        return res.status(400).send({ error: 'Malformatted ID' })
-    } else if (error.name === 'ValidationError') {
-        return res.status(400).send({ error: 'ValidationError' })
-    }
+  if(error.name === 'CastError') {
+    return res.status(400).send({ error: 'Malformatted ID' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).send({ error: 'ValidationError' })
+  }
 
-    next(error)
+  next(error)
 }
 
 app.use(errorHandler)
 
 let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      date: "2022-05-30T17:30:31.098Z",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only Javascript",
-      date: "2022-05-30T18:39:34.091Z",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      date: "2022-05-30T19:20:14.298Z",
-      important: true
-    }
+  {
+    id: 1,
+    content: 'HTML is easy',
+    date: '2022-05-30T17:30:31.098Z',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only Javascript',
+    date: '2022-05-30T18:39:34.091Z',
+    important: false
+  },
+  {
+    id: 3,
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    date: '2022-05-30T19:20:14.298Z',
+    important: true
+  }
 ]
 
 app.get('/', (req,res) => {
-    res.send('<h1>Hello World</h1>')
+  res.send('<h1>Hello World</h1>')
 })
 
 app.get('/api/notes', (req, res, next) => {
-    Note.find({}).then(notes => {
-        res.json(notes)
-    })
+  Note.find({}).then(notes => {
+    res.json(notes)
+  })
     .catch(error => next(error))
 })
 
 app.get('/api/notes/:id', (req, res, next) => {
-    Note.findById(req.params.id).then(note => {
-        if(note) {
-            res.json(note)
-        }
-        else {
-            res.status(404).end()
-        }
-    }).catch(error => next(error))
+  Note.findById(req.params.id).then(note => {
+    if(note) {
+      res.json(note)
+    }
+    else {
+      res.status(404).end()
+    }
+  }).catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (req, res, next) => {
-    Note.findByIdAndRemove(req.params.id)
-        .then( result => {
-            res.status(204).end()
-        })
-        .catch(error => next(error))
+  Note.findByIdAndRemove(req.params.id)
+    .then( result => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
 
 })
 
 app.post('/api/notes', (req, res, next) => {
-    const body = req.body
+  const body = req.body
 
-    if(!body.content) {
-        return res.status(400).json({
-            error: 'content missing'
-        })
-    }
+  if(!body.content) {
+    return res.status(400).json({
+      error: 'content missing'
+    })
+  }
 
-    const note = new Note({
-        content: body.content,
-        important: body.important || false,
-        date: new Date(),
-    })
-    
-    note.save().then(savedNote=> {
-        res.json(savedNote)
-    })
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+  })
+
+  note.save().then(savedNote => {
+    res.json(savedNote)
+  })
     .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (req, res, next) => {
-    const {content, important} = req.body
-  
-    Note.findByIdAndUpdate(req.params.id, {content, important}, { new: true, runValidators: true, context: 'query' })
-      .then(updatedNote => {
-        res.json(updatedNote)
-      })
-      .catch(error => next(error))
+  const { content, important } = req.body
+
+  Note.findByIdAndUpdate(req.params.id, { content, important }, { new: true, runValidators: true, context: 'query' })
+    .then(updatedNote => {
+      res.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`server running on port ${PORT}`);
+  console.log(`server running on port ${PORT}`)
 })
