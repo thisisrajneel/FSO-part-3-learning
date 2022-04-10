ca697298ea86a0f40de7ebd5eb4fd3ab4969c720
Rajneel Anuraj <rajneelanuraj2001@gmail.com>
2022-04-10 23:13:56 +0530

    added tests

---
+++ utils/for_testing.js
@@ -0,0 +1,17 @@
+const reverse = (string) => {
+    return string.split('')
+            .reverse('')
+            .join('')
+}
+
+const average = (array) => {
+    const reducer = (sum, num) => {
+        return sum + num
+    }
+
+    return array.length == 0 ? 0 : array.reduce(reducer, 0)/array.length
+}
+
+module.exports = {
+    reverse, average
+}
